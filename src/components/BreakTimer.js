bc4f035562b295691cf94318e0c21899f6fbcb5e
priyanka19697 <priyanka19697@gmail.com>
2019-10-11 11:28:42 +0530

    breaktimer

--- src/components/BreakTimer.js
+++ src/components/BreakTimer.js
@@ -1,9 +1,26 @@
 import React, { Component } from "react";
 
 class BreakTimer extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+
+    this.handleIncrement = this.handleIncrement.bind(this);
+    this.handleDecrement = this.handleDecrement.bind(this);
+  }
+
+  handleIncrement() {
+    this.props.increaseTime();
+    if (this.props.timerRunning === false) {
+      this.props.setCurrentTime(this.props.breaktime);
+    }
+  }
+
+  handleDecrement() {
+    this.props.decreaseTime();
+    if (this.props.timerRunning === false) {
+      this.props.setCurrentTime(this.props.breaktime);
+    }
+  }
 
   render() {
     return (
@@ -11,11 +28,11 @@ class BreakTimer extends Component {
         <div>{this.props.breaktime}</div>
         <div>Break Time</div>
         <div className="buttongroup">
-          <button onClick={this.props.increaseTime}>+</button>
+          <button onClick={this.handleIncrement}>+</button>
           <span>
             <button onClick={this.props.reset}>*</button>
           </span>
-          <button onClick={this.props.decreaseTime}>-</button>
+          <button onClick={this.handleDecrement}>-</button>
         </div>
       </div>
     );
