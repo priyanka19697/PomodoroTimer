f48dabef073e857cd0d9686048b6ae716aaba063
priyanka19697 <priyanka19697@gmail.com>
2019-10-16 14:06:24 +0530

    destructured props2

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,19 +3,25 @@ import PlayPauseButton from "./PlayPauseButton";
 
 class Timer extends Component {
   startCountDown = () => {
-    const { setTimerRunning, session, resume } = this.props;
-    const time = this.props.currentTime.split(":").map(function(item) {
+    const {
+      setTimerRunning,
+      session,
+      resume,
+      currentTime,
+      startTimer
+    } = this.props;
+    const time = currentTime.split(":").map(function(item) {
       return item.trim();
     });
     setTimerRunning();
     if (resume === false) {
       session === "work"
-        ? this.props.startTimer("" + this.props.tasktime, "00")
-        : this.props.startTimer("" + this.props.breaktime, "00");
+        ? startTimer("" + this.props.tasktime, "00")
+        : startTimer("" + this.props.breaktime, "00");
     } else {
       session === "work"
-        ? this.props.startTimer(time[0], time[1])
-        : this.props.startTimer(time[0], time[1]);
+        ? startTimer(time[0], time[1])
+        : startTimer(time[0], time[1]);
     }
     console.log(this.props, "from timer");
   };
